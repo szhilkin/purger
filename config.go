88c084d3202fafa682885fa94c7b132772d99491
Сергей <sergej@Mac-mini-Sergej.local>
2018-11-22 01:38:46 +0300

    Temporary

--- config.go
+++ config.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
-//	"path/filepath"
-//	"sort"
-//	"strings"
+	//	"path/filepath"
+	//	"sort"
+	//	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -17,19 +17,20 @@ var (
 )
 
 type Host struct {
-	Hostname  []string `yaml:"name"`
-	Retention []int `yaml:"retention"`
+	Hostgroup []string `yaml:"group"`
+	Retention string   `yaml:"retention"`
 }
 
 type Config struct {
-	Hosts         []Host  `yaml:"hosts"`
-	Aggregator    string  `yaml:"aggregator"`
-	LogPath       string  `yaml:"logpath"`
-	LogLevel      string  `yaml:"loglevel"`
-	LogFilename   string  `yaml:"log_filename"`
-	LogMaxSize    int     `yaml:"log_max_size"`
-	LogMaxBackups int     `yaml:"log_max_backups"`
-	LogMaxAge     int     `yaml:"log_max_age"`
+	Hosts            []Host `yaml:"hosts"`
+	Aggregator       string `yaml:"aggregator"`
+	DefaultRetention string `yams:"retention"`
+	LogPath          string `yaml:"logpath"`
+	LogLevel         string `yaml:"loglevel"`
+	LogFilename      string `yaml:"log_filename"`
+	LogMaxSize       int    `yaml:"log_max_size"`
+	LogMaxBackups    int    `yaml:"log_max_backups"`
+	LogMaxAge        int    `yaml:"log_max_age"`
 }
 
 func readConfig(ConfigName string) (x *Config, err error) {
@@ -47,9 +48,6 @@ func readConfig(ConfigName string) (x *Config, err error) {
 	return x, nil
 }
 
-//Проверяет время изменения конфигурационного файла
-//и перезагружает его если он изменился
-//Возвращает errNotModified если изменений нет
 func reloadConfig(configName string) (cfg *Config, err error) {
 	info, err := os.Stat(configName)
 	if err != nil {
@@ -65,70 +63,3 @@ func reloadConfig(configName string) (cfg *Config, err error) {
 	}
 	return nil, errNotModified
 }
-
-
-//проверяет подходит ли файл под маски данного правила
-//возвращает список масок
-//func (r CopyRule) match(srcFile string) (bool, []string) {
-//	var masks []string
-//	for _, mask := range r.Masks {
-//		matched, err := filepath.Match(strings.ToLower(mask), strings.ToLower(srcFile))
-//		if err != nil {
-//			errorf("Ошибка проверки MASK (%s). %s", mask, err)
-//			continue
-//		}
-//		if matched {
-//			masks = append(masks, mask)
-//		}
-//	}
-//	if len(masks) == 0 {
-//		return false, masks
-//	}
-//	return true, masks
-//}
-
-/*проверки на исключение*/
-//проверка глобального списка исключений
-//func (c Config) matchExclude(srcFile string) bool {
-//	for _, mask := range c.GlobalExcludeMasks {
-//		matched, err := filepath.Match(strings.ToLower(mask), strings.ToLower(srcFile))
-//		if err != nil {
-//			errorf("Ошибка проверки MASK (%s). %s", mask, err)
-//			continue
-//		}
-//		if matched {
-//			return true
-//		}
-//	}
-//	return false
-//}
-
-//проверка списка исключений группы
-//func (sd ScanGroup) matchExclude(srcFile string) bool {
-//	for _, mask := range sd.ExcludeMasks {
-//		matched, err := filepath.Match(strings.ToLower(mask), strings.ToLower(srcFile))
-//		if err != nil {
-//			errorf("Ошибка проверки MASK (%s). %s", mask, err)
-//			continue
-//		}
-//		if matched {
-//			return true
-//		}
-//	}
-//	return false
-//}
-
-//проверка списка исключений правила
-//func (r CopyRule) matchExclude(srcFile string) bool {
-//	for _, mask := range r.ExcludeMasks {
-//		matched, err := filepath.Match(strings.ToLower(mask), strings.ToLower(srcFile))
-//		if err != nil {
-//			errorf("Ошибка проверки MASK (%s). %s", mask, err)
-//			continue
-//		}
-//		if matched {
-//			return true
-//		}
-//	}
-//	return false
-//}
--- purge.go
+++ purge.go
@@ -13,12 +13,9 @@ const (
 )
 
 var (
-	//контролируем число потоков processScanGroup
 	tokens    chan struct{}
 	cfg       *Config
 	startTime = time.Now().Round(time.Second)
-	//эти переменные заполняются линкером.
-	//чтобы их передать надо компилировать программу с ключами
 	//go build -ldflags "-X main.buildtime '2015-12-22' -X main.version 'v1.0'"
 	version   = "debug build"
 	buildtime = "n/a"
@@ -45,28 +42,31 @@ func main() {
 	if err != nil {
 		log.Fatalf("Can't open %s: %s", configFileName, err)
 	}
+	fmt.Printf("%+v\n", cfg)
 
 	tmpDir, err := prepareTestDirTree("dir/to/walk/skip")
 	if err != nil {
 		fmt.Printf("unable to create test dir tree: %v\n", err)
 		return
 	}
-	defer os.RemoveAll(tmpDir)
-	os.Chdir(tmpDir)
+	//defer os.RemoveAll(tmpDir)
+	//os.Chdir(tmpDir)
+	os.Chdir("/")
 
-	subDirToSkip := "skip"
+	//subDirToSkip := "skip"
 
-	fmt.Println("On Unix:")
 	err = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
-		if info.IsDir() && info.Name() == subDirToSkip {
-			fmt.Printf("skipping a dir without errors: %+v \n", info.Name())
-			return filepath.SkipDir
+		if info.IsDir() {
+			//fmt.Printf("skipping a dir without errors: %+v \n", info.Name())
+			//return filepath.SkipDir
+			//}
+			fmt.Printf("visited dir: %q with modtime %s\n", filepath.Base(path),info.ModTime())
+			return nil
 		}
-		fmt.Printf("visited file or dir: %q\n", path)
 		return nil
 	})
 	if err != nil {
